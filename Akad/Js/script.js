8f0c405ae7184a68059cd7cd77677d1aff7d5ebd
Victor-pixel98 <75060742+Victor-pixel98@users.noreply.github.com>
2021-08-17 12:22:36 +0600

    Сверстал вторую страницу
    Вторая страница Services / без адаптива

--- Akad/Js/script.js
+++ Akad/Js/script.js
@@ -10,8 +10,8 @@ if (iconMenu) {
 
     });
 }
-$('.grid').masonry({
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true
-  });
+// $('.grid').masonry({
+//     itemSelector: '.grid-item',
+//     columnWidth: '.grid-sizer',
+//     percentPosition: true
+//   });
