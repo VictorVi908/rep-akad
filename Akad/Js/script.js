ab9012a1b315929a87256430a9616ca0d23ea125
Victor-pixel98 <75060742+Victor-pixel98@users.noreply.github.com>
2021-08-19 14:57:17 +0600

    Табы
    Делаю табы

--- Akad/Js/script.js
+++ Akad/Js/script.js
@@ -3,15 +3,35 @@
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.header__menu');
 if (iconMenu) {
-    iconMenu.addEventListener('click',function(e){
+    iconMenu.addEventListener('click', function (e) {
         document.body.classList.toggle('lock');
         iconMenu.classList.toggle('active');
         menuBody.classList.toggle('active');
 
     });
 }
-// $('.grid').masonry({
-//     itemSelector: '.grid-item',
-//     columnWidth: '.grid-sizer',
-//     percentPosition: true
-//   });
+
+
+//ТАБЫ
+
+document.querySelectorAll('.menu__list-li').forEach((item) =>
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const id = e.target.getAttribute('href').replace("#", '');
+
+        document.querySelectorAll('.menu__list-li').forEach((child) => (
+            child.classList.remove('menu__list-li--picked')
+        ));
+
+        document.querySelectorAll('.grid__tabs-block').forEach((child) => (
+            child.classList.remove('grid__tabs-block--active')
+        
+        ));
+
+item.classList.add('menu__list-li--picked');
+document.getElementById(id).classList.add('grid__tabs-block--active');
+    })
+);
+
+document.querySelector('.menu__list-li').click();
