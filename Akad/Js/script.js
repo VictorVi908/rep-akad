4e4ac4bdfd407990b3a532c7a2dae98b6e274327
Victor-pixel98 <75060742+Victor-pixel98@users.noreply.github.com>
2021-08-26 11:56:03 +0600

    Сверстал Contacts
    Сверстал страницу Contacts и доделал адаптив About us

--- Akad/Js/script.js
+++ Akad/Js/script.js
@@ -57,18 +57,6 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     });
 
-    $(function () {
-    
-        $('.reviews__slider').slick({
-            arrows: false,
-            dots: true,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            // fade: true,
-            // autoplay: true,
-            // autoplaySpeed: 2000,
-        });
-    
-    });
+  
     
 })
---
+++ Akad/Js/slider.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $('.reviews__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // fade: true,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+    });
+
+});
